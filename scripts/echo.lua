f5dfe3474c7f2f7911c6672b7b339998e0bff07d
Daniel <danman113@msn.com>
2021-02-17 12:12:35 -0800

    Simple working linux x example

---
+++ scripts/echo.lua
@@ -0,0 +1,3 @@
+for i, arg in ipairs(arg) do
+  print(i .. ": " .. arg)
+end
---
+++ scripts/hello.lua
@@ -0,0 +1,8 @@
+
+-- Puts a variable in the correct locations
+recipient = arg[1]
+print("Hello " .. recipient .. "!")
+--[[
+  Usage: Says hello to whatever the first argument is
+  Args: recipient: who to say hello for
+]]
